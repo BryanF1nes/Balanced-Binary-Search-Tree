f1791d72764c818487426286502c7e27595d9bf6
Bryan Fines <bryanfinesw@gmail.com>
2023-09-21 15:12:42 -0400

    Balanced Binary Search Tree

--- bst.js
+++ bst.js
@@ -1,37 +1,47 @@
-// Node Class
 class Node {
-    constructor(data) {
-        this.data = data;
+    constructor(value) {
+        this.value = value;
         this.left = null;
         this.right = null;
     }
-}
+};
 
 class Tree {
-    constructor(arr) {
-        this.root = buildTree(arr);
+    constructor() {
+        this.root = buildTree(array);
+    }
+    // Insert node
+    insertNode(value) {
+
+    }
+
+    // Remove node
+    removeNode(value) {
+        // edge case - deleting a leaf no concern
+        // edge case - deleteing a node with (1) child the nearest parent takes over the children
+        // edge case - deleting a node with (2) children / find the next biggest
     }
 }
 
-function buildTree(arr) {
-    const sortedArray = [...new Set(arr)].sort((a, b) => a - b);
+const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 
-    function buildTreeRec(start, end) {
+function buildTree(array) {
+    const sortedArray = [...new Set(array.sort((a, b) => a - b))];
+    
+    function binaryTreeRecursive(start, end) {
         if (start > end) return null;
 
         const mid = Math.floor((start + end) / 2);
         const node = new Node(sortedArray[mid]);
-    
-        node.left = buildTreeRec(start, mid - 1);
-        node.right = buildTreeRec(mid + 1, end);
-    
+
+        node.left = binaryTreeRecursive(start, mid - 1);
+        node.right = binaryTreeRecursive(mid + 1, end);
+
         return node;
     }
-
-    const root = buildTreeRec(0, sortedArray.length - 1);
+    const root = binaryTreeRecursive(sortedArray[0], sortedArray.length - 1);
     return root;
 }
-
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
     if (node === null) {
@@ -40,22 +50,11 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
     if (node.right !== null) {
       prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
     }
-    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
     if (node.left !== null) {
       prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
     }
   };
 
-
-
-const data = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-const tree = new Tree(data);
-
-prettyPrint(tree.root)
-
-// 1: Initialize start = 0, end = length of the array – 1
-// 2: mid = (start+end)/2
-// 3: Create a tree node with mid as root (lets call it A).
-// 4: Recursively do following steps:
-// 5: Calculate mid of left subarray and make it root of left subtree of A.
-// 6: Calculate mid of right subarray and make it root of right subtree of A.
+const tree = new Tree(array);
+prettyPrint(tree.root);
